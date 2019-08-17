83f8f4bddae70f3877aa37bbe6f235c6349a204b
Ignacio Varas González <ignacio.varas.gonzalez@gmail.com>
2019-08-17 16:06:04 -0400

    adds FullCoverage

---
+++ src/FullCoverage.js
@@ -0,0 +1,44 @@
+const Product = require('./Product')
+
+class FullCoverage extends Product {
+  constructor(name, sellIn, price) {
+    let n, s, p
+
+    if(arguments.length === 2) {
+      n = "Full Coverage"
+      s = name
+      p = sellIn
+    } else {
+      n = name
+      s = sellIn
+      p = price
+    }
+
+    super(n, s, p)
+  }
+
+  updatePrice() {
+    this.adjustSellIn()
+    this.adjustPrice()
+  }
+
+  adjustPrice() {
+    let adjustedPrice, increment
+
+    if(this.sellIn < 0) {
+      increment = 2
+    } else {
+      increment = 1
+    }
+
+    adjustedPrice = this.price + increment
+
+    if(adjustedPrice > 50) {
+      this.price = 50
+    } else {
+      this.price = adjustedPrice
+    }
+  }
+}
+
+module.exports = FullCoverage
